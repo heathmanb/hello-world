ea990587a88fa60e8bbaa153a62be88969a99171
heathmanb <heathman.brad@gmail.com>
2014-07-11 22:00:42 -0500

    Chang test to assertThat.

--- HelloWorld/test/helloworld/HelloWorldTest.java
+++ HelloWorld/test/helloworld/HelloWorldTest.java
@@ -1,34 +1,33 @@
 /*
  * Copyright Heathman Innovative Solutions Jul 11, 2014.
  */
-
 package helloworld;
 
-import java.io.PrintStream;
-import org.junit.After;
 import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import static org.junit.matchers.JUnitMatchers.*;
 
 /**
  *
  * @author Brad
  */
 public class HelloWorldTest {
-	@Rule
-	public final StandardOutputStreamLog log = new StandardOutputStreamLog();
-    
+
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
     public HelloWorldTest() {
     }
+
     /**
      * Test of main method, of class HelloWorld.
      */
     @Test
     public void testMain() {
         HelloWorld.main(null);
-        assertEquals("Hello World", log.getLog().trim());
+        assertThat(log.getLog(), containsString("Hello World"));
     }
-    
+
 }
