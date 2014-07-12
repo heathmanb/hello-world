3dca811b08d9a12414b9bb52a51a4d963aeb0760
heathmanb <heathman.brad@gmail.com>
2014-07-12 09:20:13 -0500

    Add BOOT_ARGS to HelloWorld and add Test for parsing the arguments.

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -1,16 +1,27 @@
 /*
  * Copyright Heathman Innovative Solutions Jul 10, 2014.
  */
-
 package helloworld;
 
 import jade.Boot;
 
 /**
+ * Hello World application main for a JADE 'HelloWorld' container.
+ *
+ * <p>
+ * Assumes a main container is running on the host.
  *
  * @author Brad
  */
 public class HelloWorld {
+
+    public static final String[] BOOT_ARGS = new String[]{
+        "-container",
+        "-container-name", "HelloWorld",
+        "-agents",
+        "HAL:helloworld.HelloAgent"
+            +";Dave:helloworld.HelloAgent"
+    };
 
     /**
      * @param args the command line arguments
@@ -18,11 +29,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
-        Boot.main(new String[] {
-            "-container",
-            "-agents",
-            "HAL:helloworld.HelloAgent"
-        });
+        Boot.main(BOOT_ARGS);
     }
-    
+
 }
--- HelloWorld/test/helloworld/HelloWorldTest.java
+++ HelloWorld/test/helloworld/HelloWorldTest.java
@@ -3,17 +3,25 @@
  */
 package helloworld;
 
+import jade.Boot;
+import jade.core.Profile;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 import org.junit.AfterClass;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.StandardErrorStreamLog;
 import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
-import static org.junit.matchers.JUnitMatchers.both;
-import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.junit.matchers.JUnitMatchers.*;
 
 /**
  * HelloWorld integration tests.
+ *
+ * <p>
+ * Assumes a main container is running on the host.
  *
  * @author Brad
  */
@@ -28,16 +36,49 @@ public class HelloWorldTest {
      * Standard out and err tests.
      */
     @Test
-    public void system_out_test() {
+    public void main_test() {
         HelloWorld.main(null);
-        Assert.assertThat("System.out test", log.getLog(),
+        assertThat("System.out test", log.getLog(),
                 both(
                         containsString("Hello World"))
                 .and(containsString("Hello World from HAL"))
+                .and(containsString("Hello World from Dave"))
         );
-        Assert.assertThat("System.err test", errLog.getLog(),
+        assertThat("System.err test", errLog.getLog(),
                 containsString("This is JADE")
         );
+
+    }
+
+    @Test
+    public void boot_arguments_test() {
+        Properties params = bootProperties();
+//        final Set keys = params.keySet();
+//        System.out.println(keys);
+        assertTrue("Properties keys", 
+                Arrays.asList(new String[] {
+        Profile.CONTAINER_NAME,
+        Profile.MAIN,
+        Profile.AGENTS})
+                        .containsAll(bootProperties().keySet()));
+        
+        assertEquals("name of container",
+                "HelloWorld",
+                params.get(Profile.CONTAINER_NAME)
+        );
+        final List bootArgs = Arrays.asList(HelloWorld.BOOT_ARGS);
+        final int agents_pos = bootArgs.indexOf("-" + Profile.AGENTS);
+        final Object agents = params.get(Profile.AGENTS);
+        System.out.println(params);
+        assertEquals("agents list",
+                bootArgs.get(agents_pos + 1),
+                params.get(Profile.AGENTS)
+        );
+    }
+
+    private Properties bootProperties() throws IllegalArgumentException {
+        final Properties params = Boot.parseCmdLineArgs(HelloWorld.BOOT_ARGS);
+        return params;
     }
 
     @AfterClass
