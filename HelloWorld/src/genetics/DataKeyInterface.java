431b426f9d340d412a252536cb469386f405993d
heathmanb <heathman.brad@gmail.com>
2014-07-14 09:25:01 -0500

    Created 24 tests in DataKeyInterfaceTest . Change DataKeyInterface signatures and added JavaDoc.

--- HelloWorld/src/genetics/DataKeyInterface.java
+++ HelloWorld/src/genetics/DataKeyInterface.java
@@ -14,28 +14,74 @@ public interface DataKeyInterface {
 
     Class<?> getType();
 
+    /**
+     * Test that a value is valid for this enumeration key.
+     * Returns false, if value is null.
+     * 
+     * @param value Value tested
+     * @return True, if valid is not null and of the type returned by getType().
+     */
     public default boolean isValid(Object value) {
-        return getType().isInstance(value);
+        return value != null && getType().isInstance(value);
     }
 
-    public default boolean isValid(DataStore ds) {
-        return getType().isInstance(get(ds));
+    /**
+     * Extracts and validates this enumerations value from a data store.
+     * 
+     * @param ds Data store that contains the value.
+     * @return Validated value.
+     * @throws IllegalArgumentException invalid, missing or null valid
+     * @throws NullPointerException DataStore is null
+     */
+    public default boolean isValidValue(DataStore ds) {
+        return isValid(get(ds));
     }
 
+    /**
+     * Uses isValid to confirm the validity of the value. If the value is
+     * invalid IllegalArgumentException is thrown.
+     * 
+     * @param value Value validated by isValue
+     * @throws IllegalArgumentException if isValid(value) return false.
+     * @see #isValid(java.lang.Object) isValid(value)
+     */
     public default void validate(Object value) {
         if (!isValid(value)) {
             throw new IllegalArgumentException("Invalid value: " + value);
         }
     }
-
+    
+    /**
+     * Retrieve and validate a value from the data store.
+     * 
+     * @param ds DataStore value is extracted from
+     * @return Validated value from the data store.
+     * @throws IllegalArgumentException invalid, missing or null valid
+     * @throws NullPointerException DataStore is null
+     */
     public default Object get(DataStore ds) {
-        return ds.get(name());
+        Object value = ds.get(name());
+        validate(value);
+        return value;
     }
 
+    /**
+     * Validate and put a value into the data store.
+     * 
+     * @param value Value to be validated and placed in the data store.
+     * @param ds DataStore value is extracted from
+     * @throws IllegalArgumentException invalid, missing or null valid
+     * @throws NullPointerException DataStore is null
+     */
     public default void put(Object value, DataStore ds) {
         validate(value);
         ds.put(name(), value);
     }
 
+    /**
+     * Enumeration name() method.
+     * 
+     * @return Name of an enumeration object. 
+     */
     public String name();
 }
--- HelloWorld/test/genetics/DataKeyInterfaceTest.java
+++ HelloWorld/test/genetics/DataKeyInterfaceTest.java
@@ -4,15 +4,18 @@
 
 package genetics;
 
-import org.junit.Test;
+import jade.core.behaviours.DataStore;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *
  * @author Brad
  */
+
+
 public class DataKeyInterfaceTest {
-    
+
     public DataKeyInterfaceTest() {
     }
 
@@ -20,60 +23,153 @@ public class DataKeyInterfaceTest {
      * Test of getType method, of class DataKeyInterface.
      */
     @Test
-    public void testGetType() {
+    public void testGetType_a() {
+        assertEquals(Long.class, DK.a.getType());
+    }
+    @Test
+    public void testGetType_b() {
+        assertEquals(String.class, DK.b.getType());
     }
 
     /**
      * Test of isValid method, of class DataKeyInterface.
      */
     @Test
-    public void testIsValid_Object() {
+    public void testIsValid_a_valid() {
+        assertTrue(DK.a.isValid(1L));
+    }
+    @Test
+    public void testIsValid_a_invalid() {
+        assertFalse(DK.a.isValid(""));
+    }
+    @Test
+    public void testIsValid_a_null() {
+        assertFalse(DK.a.isValid(null));
     }
 
     /**
-     * Test of isValid method, of class DataKeyInterface.
+     * Test of isValidValue method, of class DataKeyInterface.
      */
+    @Test(expected = NullPointerException.class)
+    public void testIsValidValue_null_ds() {
+        DK.a.isValidValue(null);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testIsValidValue_missing_pair() {
+        DK.a.isValidValue(new DataStore());
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testIsValidValue_null_value() {
+        DataStore ds = new DataStore();
+        ds.put(DK.a.name(), null);
+        assertFalse(DK.a.isValidValue(ds));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testIsValidValue_invalid_value() {
+        DataStore ds = new DataStore();
+        ds.put(DK.a.name(), "");
+        DK.a.isValidValue(ds);
+    }
     @Test
-    public void testIsValid_DataStore() {
+    public void testIsValidValue_valid_value() {
+        DataStore ds = new DataStore();
+        ds.put(DK.a.name(), 2L);
+        assertTrue(DK.a.isValidValue(ds));
     }
 
     /**
      * Test of validate method, of class DataKeyInterface.
      */
     @Test
-    public void testValidate() {
+    public void testValidate_a_valid() {
+        DK.a.validate(1L);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testValidate_a_invalid() {
+        DK.a.validate("");
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testValidate_a_null() {
+        DK.a.validate(null);
     }
 
     /**
      * Test of get method, of class DataKeyInterface.
      */
+    @Test(expected = IllegalArgumentException.class)
+    public void testGet_a_missing() {
+        DK.a.get(new DataStore());
+    }
+    @Test(expected = NullPointerException.class)
+    public void testGet_a_ds_null() {
+        DK.a.get(null);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testGet_a_null() {
+        DataStore ds = new DataStore();
+        ds.put(DK.a.name(), null);
+        DK.a.get(ds);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testGet_a_invalid() {
+        DataStore ds = new DataStore();
+        ds.put(DK.a.name(), "");
+        DK.a.get(ds);
+    }
     @Test
-    public void testGet() {
+    public void testGet_a_valid() {
+        DataStore ds = new DataStore();
+        long expected = 2;
+        ds.put(DK.a.name(), expected);
+        assertEquals(expected, DK.a.get(ds));
     }
 
     /**
      * Test of put method, of class DataKeyInterface.
      */
+    @Test(expected = NullPointerException.class)
+    public void testPut_a_ds_null() {
+        DK.a.put(1L, null);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testPut_a_null() {
+        DK.a.put(null, new DataStore());
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testPut_a_invalid() {
+        DK.a.put("", new DataStore());
+    }
     @Test
-    public void testPut() {
+    public void testPut_a_valid() {
+        DK.a.put(2L, new DataStore());
+        assertTrue(true);
     }
 
     /**
      * Test of name method, of class DataKeyInterface.
      */
     @Test
-    public void testName() {
+    public void testName_a() {
+        assertEquals(DK.a.name(), "a");
+    }
+    @Test
+    public void testName_b() {
+        assertEquals(DK.b.name(), "b");
     }
 
-    public class DataKeyInterfaceImpl implements DataKeyInterface {
+    public enum DK implements DataKeyInterface {
 
+        a, b;
+
+        @Override
         public Class<?> getType() {
-            return null;
-        }
-
-        public String name() {
-            return "";
+            switch (this) {
+                case a:
+                    return Long.class;
+                default:
+                    return String.class;
+            }
         }
     }
-    
+
 }
